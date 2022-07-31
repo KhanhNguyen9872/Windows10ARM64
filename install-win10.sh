25f4208d292b1283359631f3085cb5afaec9b86f
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-07-31 18:04:53 +0700

    Update install-win10.sh

--- install-win10.sh
+++ install-win10.sh
@@ -21,14 +21,20 @@ clear
 cd 2> /dev/null
 printf "\n\n${light_cyan} - Downloading....${green}\n\n"
 wget -q --show-progress -O base_arm64-khanhnguyen.tar.7z.001 "https://github.com/KhanhNguyen9872/Windows10ARM64/releases/download/Win10ARM64Base/base_arm64-khanhnguyen.tar.7z.001"
-wget -q --show-progress -O base_arm64-khanhnguyen.tar.7z.002 "https://github.com/KhanhNguyen9872/Windows10ARM64/releases/download/Win10ARM64Base/base_arm64-khanhnguyen.tar.7z.002"
+if [ -f base_arm64-khanhnguyen.tar.7z.001 ]; then
+    wget -q --show-progress -O base_arm64-khanhnguyen.tar.7z.002 "https://github.com/KhanhNguyen9872/Windows10ARM64/releases/download/Win10ARM64Base/base_arm64-khanhnguyen.tar.7z.002"
+else
+    echo "Error when download file"
+    echo "Internet so bad!"
+    exit 0
+fi
 wget -q --show-progress -O base_arm64-khanhnguyen.sha512sum "https://github.com/KhanhNguyen9872/Windows10ARM64/releases/download/Win10ARM64Base/base_arm64-khanhnguyen.sha512sum" 2> /dev/null
 
 # extract file
 clear
 printf "\n\n${light_cyan} - Extracting....${green}\n\n"
 7z x base_arm64-khanhnguyen.tar.7z.001 -aoa > /dev/null
-rm -f base_arm64-khanhnguyen.7z* 2> /dev/null
+rm -rf base_arm64-khanhnguyen.7z* 2> /dev/null
 
 # Verify base file
 clear
